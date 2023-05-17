5a2bdac83ce4e652e055ef44d5cd658ca3cedfd7
JulSanz <131091145+JulSanz@users.noreply.github.com>
2023-05-17 17:01:07 -0600

    Update ejercicio_002.py

--- ejercicio_002.py
+++ ejercicio_002.py
@@ -2,11 +2,14 @@ nombre = input("Ingresa tu Nombre ")
 sexo = input("Ingresa tu sexo M/F ")
 
 
+nombreupper = nombre.upper()
 
-
-if (sexo == "M" and nombre >= "N") or (sexo == "F" and nombre < "M"):
+if (sexo == "M" and nombreupper >= "N") or (sexo == "F" and nombreupper < "M"):
     print("Grupo A")
 else:
     print("Grupo B")
+
+
+print(nombreupper)
 
 
