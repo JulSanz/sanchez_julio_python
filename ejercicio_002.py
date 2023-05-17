c72d2c7f519242bf307989e5e396ada03a70b5d4
JulSanz <131091145+JulSanz@users.noreply.github.com>
2023-05-17 17:06:03 -0600

    Update ejercicio_002.py

--- ejercicio_002.py
+++ ejercicio_002.py
@@ -1,9 +1,12 @@
+#Definir entradas
 nombre = input("Ingresa tu Nombre ")
 sexo = input("Ingresa tu sexo M/F ")
 
-
+#Validacion de texto mayusculas
 nombreupper = nombre.upper()
 
+
+#Condicional para dividir grupos
 if (sexo == "M" and nombreupper >= "N") or (sexo == "F" and nombreupper < "M"):
     print("Grupo A")
 else:
