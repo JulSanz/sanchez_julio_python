ed535c5a506fba90478a1a006d7c7b2bf7e24766
JulSanz <131091145+JulSanz@users.noreply.github.com>
2023-05-17 17:07:15 -0600

    Update ejercicio_003.py

--- ejercicio_003.py
+++ ejercicio_003.py
@@ -1,9 +1,12 @@
+#Entrada de Paswword
 password_master = input(" Crea tu password ")
 
 
-
+#Ciclo para validar password
 while True:
+    #Pedir nuevo pasword, tiene que estar dentro del bucle para que no se retroalimente
     password_request = input(" Ingresa tu password ")
+    #Condicional
     if password_master == password_request:
         print("Bienvenido")
         break
