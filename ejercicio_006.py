77a63b01ca04e2b86ae9ac954c733cba8c574057
JulSanz <131091145+JulSanz@users.noreply.github.com>
2023-05-17 18:10:12 -0600

    Update ejercicio_006.py

--- ejercicio_006.py
+++ ejercicio_006.py
@@ -5,14 +5,12 @@ pi = math.pi
 
 #Funcion para Area
 def area_circulo (radio):
-    areacic = pi * radio ** 2
-    return areacic
+    return  pi * radio ** 2
 
 #funcion para volumen
 def area_cilindro (radio , altura):
     area_base = area_circulo(radio)
-    volumencil = area_base * altura
-    return volumencil
+    return area_base * altura
 
 #Pedir radio
 radio = float(input("Cual es el radio del circulo? "))
@@ -23,4 +21,3 @@ print("El area del circulo es: ", round(area,2), "metros cuadrados")
 altura = float(input("Cual es la altura del cilindro? "))
 volumen = area_cilindro(radio, altura)
 print("El volumen del cilindro es: ", round(volumen,2) , "metros cubicos")
-
